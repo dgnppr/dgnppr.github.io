49f44856f8084cbb9710a37b66e4f6b3d6d1fc25
Yonghyun Yoon <dgnppr@gmail.com>
2024-02-20 04:00:39 +0900

    스크롤 프로그레스바 모바일 적용

--- js/scroll.js
+++ js/scroll.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // 프로그래스 바 엘리먼트
   const progressBar = document.getElementById("myBar");
+  let isScrolling;
 
-  // window 객체가 스크롤될 경우
-  // window 스크롤 이벤트 감지 및 콜백 셋팅
-  window.addEventListener("scroll", function () {
-    // window의 스크롤 진행도 계산
+  function updateProgressBar() {
     const scrollHeight = document.documentElement.scrollHeight - window.innerHeight;
     const scrolled = (window.scrollY / scrollHeight) * 100;
-
-    // 계산된 스크롤 진행도를 CSS로 표현
     progressBar.style.width = scrolled + "%";
-  });
+  }
+
+  window.addEventListener("scroll", function () {
+    window.cancelAnimationFrame(isScrolling);
+    isScrolling = window.requestAnimationFrame(updateProgressBar);
+  }, { passive: true });
 });
